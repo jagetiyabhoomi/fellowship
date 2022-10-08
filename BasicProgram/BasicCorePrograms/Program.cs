fd0e96bb643da30814a5d9147f280bc54beca079
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-08 09:22:14 +0530

    Call Largest Number

---
+++ BasicProgram/BasicCorePrograms/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BasicCorePrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //VowelAndConsonant.CheckTheTypeOfCharacter();
+            //SwapTwoNumbers.SwapNumber();
+            //LargestNumber.CheckLargestNumber();
+            //QuotientRemainder.CheckQuotientRemainder();
+            //OddOrEven.CheckOddEvenNumber();
+            //PrimeFactor.FindPrimeFactor();
+            HarmonicNumber.FindHarmonicNumber();
+        }
+    }
+}
